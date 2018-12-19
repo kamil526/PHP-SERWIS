897e115fd34041eadd8c4d6612b87db4212b03a8
kamil526 <kporeba526@gmail.com>
2018-12-19 15:59:52 +0100

    zmiany wizualne w kodzie...

--- register.php
+++ register.php
@@ -7,34 +7,34 @@
         // jeśli zostanie naciśnięty przycisk "Zarejestruj"
         if(isset($_POST['login'])) {
             //pobieramy dane
-            $_POST['login'] = $_POST['login'] ?? '';
-            $_POST['password'] = $_POST['password'];
-            $_POST['password2'] = $_POST['password2'];
-            $_POST['imie'] = $_POST['imie'];
-            $_POST['nazwisko'] = $_POST['nazwisko'];
-            $_POST['email'] = $_POST['email'];
-            $_POST['telefon'] = $_POST['telefon'];
+            $login = $_POST['login'] ?? '';
+            $password = $_POST['password'];
+            $password2 = $_POST['password2'];
+            $imie = $_POST['imie'];
+            $nazwisko = $_POST['nazwisko'];
+            $email = $_POST['email'];
+            $telefon = $_POST['telefon'];
              //dane adresowe
-            $_POST['ulica'] = $_POST['ulica'];
-            $_POST['nrDomu'] = $_POST['nrDomu'];
-            $_POST['kodPocztowy'] = $_POST['kodPocztowy'];
-            $_POST['miasto'] = $_POST['miasto'];
+            $ulica = $_POST['ulica'];
+            $nrDomu = $_POST['nrDomu'];
+            $kodPocztowy = $_POST['kodPocztowy'];
+            $miasto = $_POST['miasto'];
             //dane firmy
-            $_POST['nip'] = $_POST['nip'];
-            $_POST['nazwaFirmy'] = $_POST['nazwaFirmy'];
-            $_POST['nazwaBanku'] = $_POST['nazwaBanku'];
-            $_POST['kontoBankowe'] = $_POST['kontoBankowe'];
+            $nip = $_POST['nip'];
+            $nazwaFirmy = $_POST['nazwaFirmy'];
+            $nazwaBanku = $_POST['nazwaBanku'];
+            $kontoBankowe = $_POST['kontoBankowe'];
             
     
             // sprawdzamy czy wszystkie pola zostały wypełnione
-            if(empty($_POST['login']) || empty($_POST['password']) || empty($_POST['password2'])) {
+            if(empty($login) || empty($password) || empty($password2)) {
                 echo '<p>Musisz wypełnić wszystkie obowiązkowe pola.</p>';
             // sprawdzamy czy podane dwa hasła są takie same
-            } elseif($_POST['password'] != $_POST['password2']) {
+            } elseif($password != $password2) {
                 echo '<p>Podane hasła różnią się od siebie.</p>';
             } else {
                 // sprawdzamy czy są jacyś uzytkownicy z takim loginem lub adresem email
-                $sql="SELECT Count(idKlienta) FROM Klienci WHERE login = '{$_POST['login']}'";
+                $sql="SELECT Count(idKlienta) FROM Klienci WHERE login = '$login'";
                 $result = mysqli_query($polaczenie, $sql);
                 $row = mysqli_fetch_row($result);
                 if($row[0] > 0) {
@@ -43,7 +43,7 @@
                     // jeśli nie istnieje to kodujemy haslo...
                     $_POST['password'] = hashHaslo($_POST['password']);
                     // i wykonujemy zapytanie na dodanie usera
-                    $sql="INSERT INTO Klienci (login, haslo) VALUES ('{$_POST['login']}', '{$_POST['password']}')";
+                    $sql="INSERT INTO Klienci (login, haslo) VALUES ('$login', '{$password}')";
                     mysqli_query($polaczenie, $sql);
                     echo '<p>Zostałeś poprawnie zarejestrowany! Możesz się teraz <a href="loginKlient.php">zalogować</a>.</p>';
                 }
@@ -54,53 +54,51 @@
         echo '<form method="post" action="register.php">
             <p>
                 Login:<br>
-                <input type="text" value="'.$_POST['login'].'" name="login">
+                <input type="text" value="'.$login.'" name="login" >
             </p>
+
             <p>
                 Hasło:<br>
-                <input type="password" value="'.$_POST['password'].'" name="password">
+                <input type="password" value="'.$password.'" name="password">
             </p>
             <p>
                 Powtórz hasło:<br>
-                <input type="password" value="'.$_POST['password2'].'" name="password2">
+                <input type="password" value="'.$password2.'" name="password2">
             </p>
-
-
-
 
 
             <p>
                 Imie:<br>
-                <input type="text" value="'.$_POST['imie'].'" name="imie">
+                <input type="text" value="'.$imie.'" name="imie">
             </p>
             <p>
                 Nazwisko:<br>
-                <input type="text" value="'.$_POST['nazwisko'].'" name="nazwisko">
+                <input type="text" value="'.$nazwisko.'" name="nazwisko">
             </p>
             <p>
                 Email:<br>
-                <input type="text" value="'.$_POST['email'].'" name="email">
+                <input type="text" value="'.$email.'" name="email">
             </p>
             <p>
                 Telefon:<br>
-                <input type="text" value="'.$_POST['telefon'].'" name="telefon">
+                <input type="text" value="'.$telefon.'" name="telefon">
             </p>
             <p>
                 Nowe:<br>
-                <input type="text" value="'.$_POST['nowe'].'" name="nowe">
+                <input type="text" value="'.$nowe.'" name="nowe">
             </p>
             <p>
                 Nowe:<br>
-                <input type="text" value="'.$_POST['nowe'].'" name="nowe">
+                <input type="text" value="'.$nowe.'" name="nowe">
             </p>
             <p>
                 Nowe:<br>
-                <input type="text" value="'.$_POST['nowe'].'" name="nowe">
+                <input type="text" value="'.$nowe.'" name="nowe">
             </p>
 
 
             <p>
-                <input type="submit" value="Zarejestruj">
+                <input type="submit" value="Zarejestruj" name="zarejestruj">
             </p>
         </form>';
     } else {
