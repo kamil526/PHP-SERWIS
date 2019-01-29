302d7af6a81cda3d19b2a82ba5c2c35203bafdfb
kamil526 <kporeba526@gmail.com>
2019-01-29 17:23:41 +0100

    przebudowa

--- loginKlient.php
+++ loginKlient.php
@@ -1,107 +1,77 @@
-
-
-
 <?php
+    session_start();
     include 'config.php';
     otworzPoloczenie();
-    session_start();
 
-    // jeśli nie ma jeszcze sesji "logged" i "idKlienta" to wypełniamy je domyślnymi danymi
-    if(!isset($_SESSION['logged'])) {
-        $_SESSION['logged'] = false;
-        $_SESSION['idKlienta'] = -1;
-    }
-    
-    // sprawdzamy czy user nie jest przypadkiem zalogowany
-    if(!$_SESSION['logged']) {
-        // jeśli zostanie naciśnięty przycisk "Zaloguj"
-        if(isset($_POST['login'])) {
-            // filtrujemy dane...
-            $login = $_POST['login'] ?? '';
-            //$_POST['password'] = $_POST['password'];
-            $password = hashHaslo($_POST['password']) ?? '';
-            $sql="SELECT idKlienta FROM Klienci WHERE login = '$login' AND haslo = '$password' LIMIT 1";
-            // sprawdzamy prostym zapytaniem sql czy podane dane są prawidłowe
-            $result = mysqli_query($polaczenie, $sql);
-            if(mysqli_num_rows($result) > 0) {
-                // jeśli tak to ustawiamy sesje "logged" na true oraz do sesji "idKlienta" wstawiamy idKlienta
-                $row = mysqli_fetch_assoc($result);
-                $_SESSION['logged'] = true;
-                $_SESSION['idKlienta'] = $row['idKlienta'];
-                $_SESSION['login'] = $_POST['login'];
-                header('Location: index.html');
-            } else {
+    //jezeli uzytkownik jest zalogowany, przekieruj go na index.html
+    if ((isset($_SESSION['logged'])) && ($_SESSION['logged']==true)){
+        header('Location: index.html');
+        //nie wykonuj kodu poniżej
+		exit();
+	}else{
+            // jeśli nie ma jeszcze sesji "logged" i "idKlienta" to wypełniamy je domyślnymi danymi
+            $_SESSION['logged'] = false;
+            $_SESSION['idKlienta'] = -1;
+        }
+?>
+      
+<!doctype html>
+<html lang="pl">
+    <head>
+        <title>eSerwis - Logowanie Klienta </title>
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="startpage/css/styles.css">
+        <link rel="stylesheet" href="startpage/css/bootstrap.css">
+    </head>
+    <body>
+        <form method="post" action="loginKlient.php">
+            <center>
+                <br>
+                <br>
+                <br>
+                <p>
+                    Login:<br>
+                    <input type="text" name="login">
+                </p>
+                <p>
+                    Hasło:<br>
+                    <input type="password" name="password">
+                </p>
+                <p>
+                    <input type="submit" value="Zaloguj się">
+                </p>
+                <br>
+                <p> 
+                    [<a href="register.php">Nie posiadasz konta? Zarejestruj się!</a>]
+                </p>
+            </center>
+        </form>
+    </body>
+</html>
+
+<?php
+    // jeśli zostanie naciśnięty przycisk "Zaloguj"
+    if(isset($_POST['login'])) {
+        // filtrujemy dane...
+        $login = $_POST['login'] ?? '';
+        //$_POST['password'] = $_POST['password'];
+        $password = hashHaslo($_POST['password']) ?? '';
+        $sql="SELECT idKlienta FROM Klienci WHERE login = '$login' AND haslo = '$password' LIMIT 1";
+        // sprawdzamy prostym zapytaniem sql czy podane dane są prawidłowe
+        $result = mysqli_query($polaczenie, $sql);
+        if(mysqli_num_rows($result) > 0) {
+            // jeśli tak to ustawiamy sesje "logged" na true oraz do sesji "idKlienta" wstawiamy idKlienta
+            $row = mysqli_fetch_assoc($result);
+            $_SESSION['logged'] = true;
+            $_SESSION['idKlienta'] = $row['idKlienta'];
+            $_SESSION['login'] = $_POST['login'];
+            header('Location: index.html');
+        } else {
                 echo '<p>Podany login i/lub hasło jest nieprawidłowe.</p>';
             }
-        }
-    
-        // wyświetlamy komunikat na zalogowanie się
-        echo 
-        '
-            <html>
-                <head>
-                    <meta charset="UTF-8">
-                    <title>
-                        Zaloguj się
-                    </title>
-                </head>
-                <body>
-                    <form method="post" action="loginKlient.php">
-                        <center>
-                            <br>
-                            <br>
-                            <br>
-                            <p>
-                                Login:<br>
-                                <input type="text" value="'.($login ?? '').'" name="login">
-                            </p>
-                            <p>
-                                Hasło:<br>
-                                <input type="password" value="'.($password ?? '').'" name="password">
-                            </p>
-                            <p>
-                                <input type="submit" value="Zaloguj">
-                            </p>
-                            <br>
-                            <p> 
-                                [<a href="register.php">Nie posiadasz konta? Zarejestruj się!</a>]
-                            </p>
-                        </center>
-                    </form>
-                </body>
-            </html>
-        '; 
-    } else {
-            echo 
-            '
-                <html>
-                    <head>
-                        <meta charset="UTF-8">
-                        <title>
-                            Logowanie
-                        </title>
-                    </head>
-                    <body>
-                        <form method="POST" action="logout.php">
-                        
-                            <center>
-                                <br>
-                                <br>
-                                <br>
-                                <p>
-                                    <b>'.$_SESSION['login'].'</b>, Jesteś już zalogowany, więc nie możesz się zalogować ponownie.
-                                </p>
-                                <p>
-                                    [<a href="index.html">Powrót</a>]
-                                </p>
-                                <p>
-                                    <input type="submit" value="Wyloguj" name="wyloguj">
-                                </p>
-                            </center>
-                        </form>
-                    </body>
-                </html>
-            ';
-        }
+    }
+
     zamknijPoloczenie();
 ?>
