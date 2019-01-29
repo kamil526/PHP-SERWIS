0b80725557031ee047262096aba9ae83c483f973
kamil526 <kporeba526@gmail.com>
2019-01-29 17:23:55 +0100

    no message

--- logout.php
+++ logout.php
@@ -2,12 +2,6 @@
     
     include 'config.php';
     session_start();
-
-    if(!$_SESSION['logged']) {
-        echo '<p>Nie jesteś zalogowany.</p>';
-    }else {
-            $_SESSION['logged'] = false;
-            $_SESSION['idKlienta'] = -1;
-            echo '<center><p>Zostałeś wylogowany! Możesz teraz przejść na <a href="index.html">stronę główną</a>.</p></center>';;
-        }
+    session_unset();
+    echo 'Zostałeś wylogowany!';
 ?>
