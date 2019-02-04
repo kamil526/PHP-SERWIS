0c1cb59eefe8a577e75a5c73d1fb49ce7bb74645
kamil526 <kporeba526@gmail.com>
2019-02-05 00:24:32 +0100

    podpięcie styli, bindowanie pól, zapis do bazy danych

--- wycena.php
+++ wycena.php
@@ -3,48 +3,6 @@
     otworzPoloczenie();
 ?>
 
-
-
-<?php   
-    // jeśli zostanie naciśnięty przycisk "Zapisz"
-    if(isset($_POST['login'])) {
-        //pobieramy dane z pól
-        $marka = $_POST['marka'] ?? '';
-        $model = $_POST['model'] ?? '';
-        $opisUsterki = $_POST['opisUsterki'] ?? '';
-        $opisZlecenia = $_POST['opisZlecenia'] ?? '';
-        $dataPrzekazaniaPojazdu = $_POST['dataPrzekazaniaPojazdu'] ?? '';
-        // sprawdzamy czy wszystkie pola zostały wypełnione
-        if(empty($login) || empty($password) || empty($password2)) {
-            echo '<p>Musisz wypełnić wszystkie obowiązkowe pola.</p>';
-        // sprawdzamy czy podane dwa hasła są takie same
-        } elseif($password != $password2) {
-            echo '<p>Podane hasła różnią się od siebie.</p>';
-        } else {
-            // sprawdzamy czy są jacyś uzytkownicy z takim loginem lub adresem email
-            $sql="SELECT Count(idKlienta) FROM Klienci WHERE login = '$login'";
-            $result = mysqli_query($polaczenie, $sql);
-            $row = mysqli_fetch_row($result);
-            if($row[0] > 0) {
-                echo '<p>Już istnieje użytkownik z takim loginem.</p>';
-            } else {
-                // jeśli nie istnieje to kodujemy haslo...
-                $password = hashHaslo($_POST['password']);
-                // i wykonujemy zapytanie na dodanie usera
-                $sql="INSERT INTO Klienci (login, haslo, imie, nazwisko, email, telefon, ulica
-                                            , nrDomu, kodPocztowy, miasto, nip, nazwaFirmy, nazwaBanku)
-                    VALUES ('$login', '$password', '$imie', '$nazwisko', '$email','$telefon', '$ulica'
-                            , '$nrDomu', '$kodPocztowy', '$miasto', '$nip', '$nazwaFirmy', '$nazwaBanku')";
-                mysqli_query($polaczenie, $sql);
-                
-                echo '<p>Zostałeś poprawnie zarejestrowany! Możesz się teraz <a href="loginKlient.php">zalogować</a>.</p>';
-                //zamknijPoloczenie();
-            }
-        }
-    }  
-?>
-
-
 <div class="container">
     <div class="row featurette">
 
@@ -54,20 +12,20 @@
             <button class="btn btn-light" type="submit">Moje dane</button>
         </div>
 
-        <div class="col-md-10" form>
+        <form class="col-md-10" method="post" action="wycena.php" >
             <div class="row featurette">
 
                 <div class="col-md-5">
                     <div class="form-group">
-                        <label for="inputAddress">Marka:</label>
+                        <label class="w3-text-green"><b>Marka:</b></label>
                         <input type="text" class="form-control" name="marka" placeholder="Marka pojazdu">
                     </div>
                     <div class="form-group">
-                        <label for="inputAddress">Model:</label>
+                        <label class="w3-text-green"><b>Model:</b></label>
                         <input type="text" class="form-control" name="model" placeholder="Model pojazdu">
                     </div>
                     <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Opis usterki:</label>
+                        <label class="w3-text-green"><b>Opis usterki:</b></label>
                         <textarea class="form-control" name="opisUsterki" 
                         placeholder="Tutaj szczegółowo usterkę pojazdu, na jej podstawie, oszacujemy koszt naprawy" rows="8"></textarea>
                     </div>
@@ -75,13 +33,13 @@
 
                 <div class="col-md-5">
                     <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Opis zlecenia:</label>
+                        <label class="w3-text-green"><b>Opis zlecenia:</b></label>
                         <textarea class="form-control" name="opisZlecenia" 
                         placeholder="Tutaj opisz na czym ma polgać zlecenie" rows="6"></textarea>
                     </div>
 
                     <div class="form-group">
-                        <label for="exampleFormControlSelect1">Data przekazania pojazdu:</label>
+                        <label class="w3-text-green"><b>Data przekazania pojazdu:</b></label>
                         <input type="date" name="dataPrzekazaniaPojazdu" max="3000-12-31" 
                                 min="2019-01-01" class="form-control">    
                     </div>
@@ -91,7 +49,7 @@
 
             <div class="row featurette">
                 <div class="col-md-10">
-                    <label>Tutaj możesz dołączyć zdjęcie lub inny dokument dotyczący zlecenia:</label>
+                    <label class="w3-text-green"><b>Tutaj możesz dołączyć zdjęcie lub inny dokument dotyczący zlecenia:</b></label>
                     <div class="custom-file">
                         <input type="file" class="custom-file-input" name="zdjecieUszkodzenia">
                         <label class="custom-file-label" for="customFile"></label>
@@ -101,14 +59,40 @@
             
             <div class="form-group row">
                 <div class="col-md-12">
-                    <button type="submit" class="btn btn-primary">Zapisz</button>
+                    <button type="submit" class="w3-btn w3-green"name="zapisz">Zapisz</button>
                 </div>
             </div>
             
-        </div>
+        </form>
+
 
     </div>
 </div>
+
+<?php   
+    $idKlienta =$_SESSION['idKlienta'] ?? '';
+
+    if(isset($_POST['zapisz'])&&(isset($_SESSION['logged'])) && ($_SESSION['logged']==true)) {
+        //pobieramy dane z pól
+        $marka = $_POST['marka'] ?? '';
+        $model = $_POST['model'] ?? '';
+        $opisUsterki = $_POST['opisUsterki'] ?? '';
+        $opisZlecenia = $_POST['opisZlecenia'] ?? '';
+        $dataPrzekazaniaPojazdu = $_POST['dataPrzekazaniaPojazdu'] ?? '';
+        // sprawdzamy czy wszystkie pola zostały wypełnione
+        if(empty($marka) || empty($model) || empty($opisUsterki) || empty($opisZlecenia) || empty($dataPrzekazaniaPojazdu)) {
+            echo '<p><center>Musisz wypełnić wszystkie obowiązkowe pola</center>.</p>';
+        } else{
+                $sql="INSERT INTO Zlecenia (dataDodania, idKlienta, idPracownika, 
+                        opisZlecenia, statusZlecenia, wartoscZlecenia)
+                        VALUES (now(), $idKlienta, 1, '$opisZlecenia', 1, 0)";
+                mysqli_query($polaczenie, $sql);
+                
+                echo '<p><center>Zlecenie zostało dodane</center></p>';
+                //zamknijPoloczenie();
+        }
+    }
+?>
 
 <?php
     include 'bottomPage.php';
