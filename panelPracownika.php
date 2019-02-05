08369932d90a837bf00686d670465f32785667e6
michalSRT <m.sierotowicz89@gmail.com>
2019-02-06 00:44:31 +0100

    no message

--- panelPracownika.php
+++ panelPracownika.php
@@ -6,7 +6,7 @@
 <div class="row">
     <div class="col-sm-2">
         <div class=" w3-bar-block w3-large" > 
-            <button type="button" class="w3-btn w3-hover-green" data-toggle="modal" data-target="#exampleModalLong">Nowe zlecenie</button>
+            <button type="button" class="w3-btn w3-hover-green" data-toggle="modal" data-target=".bd-example-modal-lg">Nowe zlecenie</button>
             <br><br><br>
             <form><input type="button" value="Dodaj pracownika" onclick="window.location.href='registerPracownik.php'" class="w3-btn w3-hover-green" /></form>
         </div>
@@ -79,24 +79,89 @@
     </div>
 </div>
 
-<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Nowe zlecenie</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="w3-btn" data-dismiss="modal">Close</button>
-        <button type="button" class="w3-btn w3-green">Save changes</button>
-      </div>
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" 
+                                                aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Nowe zlecenie</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <!-- Zawartosc modala -->
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <label class="w3-text-green" ><b>Data dodania:</b></label>
+                        <input type="date" name="dataDodania" max="3000-12-31" min="2019-01-01" class="form-control"> 
+                    </div>
+                    <div class="col-md-4">
+                        <label class="w3-text-green" ><b>Data przekazania:</b></label>
+                        <input type="date" name="dataPrzekazania" max="3000-12-31" min="2019-01-01" class="form-control"> 
+                    </div>
+                    <div class="col-md-4">
+                        <label class="w3-text-green" ><b>Data zakończenia:</b></label>
+                        <input type="date" name="dataZakonczenia" max="3000-12-31" min="2019-01-01" class="form-control"> 
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-md-4">
+                        <label class="w3-text-green" ><b>Rodzaj zlecenia:</b></label>
+                        <textarea class="form-control" name="rodzajZlecenia" rows="3"></textarea>
+                    </div>
+                    <div class="col-md-4">
+                        <label class="w3-text-green" ><b>Opis zlecenia:</b></label>
+                        <textarea class="form-control" name="opisZlecenia" rows="3"></textarea>
+                    </div>
+                    <div class="col-md-4">
+                        <label class="w3-text-green" ><b>Opis usterki:</b></label>
+                        <textarea class="form-control" name="opisUsterki" rows="3"></textarea>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-md-4">   
+                        <label class="w3-text-green"><b>Marka:</b></label>
+                        <input type="text" class="form-control" name="markaPojazdu" placeholder="Marka pojazdu">
+                    </div> 
+                    <div class="col-md-4">   
+                        <label class="w3-text-green"><b>Model:</b></label>
+                        <input type="text" class="form-control" name="modelPojazdu" placeholder="Model pojazdu">
+                    </div> 
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-md-4">   
+                        <label class="w3-text-green"><b>Klient:</b></label>
+                        <input type="text" class="form-control" name="idKlienta">
+                    </div> 
+                    <div class="col-md-4">   
+                        <label class="w3-text-green"><b>Wartość zlecenia:</b></label>
+                        <input type="text" class="form-control" name="wartoscZlecenia">
+                    </div> 
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-md-12">   
+                        <label class="w3-text-green"><b>Komentarz pracownika:</b></label>
+                        <textarea class="form-control" name="komentarzPracownika" rows="4"></textarea>
+                    </div> 
+                </div>
+
+
+            </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="w3-btn" data-dismiss="modal">Close</button>
+                <button type="button" class="w3-btn w3-green">Save changes</button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 
