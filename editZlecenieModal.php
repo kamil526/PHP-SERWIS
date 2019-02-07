5d391b9444a21460609362e4a3d0af2967cfb470
michalSRT <m.sierotowicz89@gmail.com>
2019-02-07 09:23:15 +0100

    no message

--- editZlecenieModal.php
+++ editZlecenieModal.php
@@ -1,25 +1,36 @@
-<div class="modal fade bd-example2-modal-lg" tabindex="-1" role="dialog" 
-    aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-    <?php
-        $idZlecenia = $_POST['idZlecenia'] ?? '';
-        if(isset($_POST['zapisz'])){
+<?php
+        if(isset($_POST['zapisz']))
+        {
             //pobieramy dane z pól
+            $idKlienta = $_SESSION['idKlienta'] ?? '';
+            $idZlecenia = $_POST['idZlecenia'] ?? '';
             $markaPojazdu = $_POST['markaPojazdu'] ?? '';
             $modelPojazdu = $_POST['modelPojazdu'] ?? '';
             $opisUsterki = $_POST['opisUsterki'] ?? '';
             $opisZlecenia = $_POST['opisZlecenia'] ?? '';
-            $dataPrzekazaniaPojazdu = $_POST['dataPrzekazaniaPojazdu'] ?? '';
+            $dataPrzekazania = $_POST['dataPrzekazania'] ?? '';
 
-            $sql="UPDATE Zlecenia set opisZlecenia='$opisZlecenia',opisUsterki='$opisUsterki',markaPojazdu='$markaPojazdu',modelPojazdu='$modelPojazdu'
-                    where idZlecenia=$idZlecenia";
-                
-                mysqli_query($polaczenie, $sql);
+            $sql="UPDATE Zlecenia SET markaPojazdu='$markaPojazdu', modelPojazdu='$modelPojazdu', opisUsterki='$opisUsterki', 
+            opisZlecenia='$opisZlecenia', dataPrzekazania='$dataPrzekazania'
+            WHERE idZlecenia='$idZlecenia'";
+            
+
+            mysqli_query($polaczenie, $sql);
+/*          if (mysqli_query($polaczenie, $sql)) {
+                echo "Record updated successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($polaczenie);
+            }   */ 
         }
-        /*
-        $select="select imie, nazwisko, email, telefon, nrDomu, kodPocztowy, ulica, miasto, nazwaFirmy, nip FROM Klienci where idKlienta=$idKlienta";
-        $result=mysqli_query($polaczenie, $select);     
-        $query=mysqli_fetch_array($result);
-        */
+?>
+
+<div class="modal fade bd-example2-modal-lg" tabindex="-1" role="dialog" 
+    aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+
+    <?php 
+                $sql2="SELECT * FROM Zlecenia WHERE idKlienta='$idKlienta'";
+                $result2 = mysqli_query($polaczenie, $sql2);
+                $query2 = mysqli_fetch_array($result2);
     ?>
 
     <div class="modal-dialog modal-lg" role="dialog">
@@ -33,23 +44,26 @@
 
             <div class="modal-body">
                 <!-- Zawartosc modala -->
-                <form method="post" action="zleceniaKlient.php" id="modal-form">
+                <form method="post" action="zleceniaKlient.php" id="modal-form2">
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-4">
                                 <label class="w3-text-green" ><b>idZlecenia:</b></label>
-                                <input type="text" class="form-control" name="idZlecenia"
-                                value="<?php echo $idZlecenia; ?>" required data-validation>
+
+                                <input type="text" class="form-control" name="idZlecenia" readonly
+                                value="<?php echo $query2['idZlecenia'];?>">
+
                             </div>
                             <div class="col-md-4">
                                 <label class="w3-text-green" ><b>Marka pojazdu:</b></label>
                                 <input type="text" class="form-control" name="markaPojazdu"
-                                value="<?php echo $markaPojazdu; ?>" required data-validation>
+                                value="<?php echo $query2['markaPojazdu']; ?>" required data-validation>
+
                             </div>
                             <div class="col-md-4">
                                 <label class="w3-text-green" ><b>Model pojazdu:</b></label>
                                 <input type="text" class="form-control" name="modelPojazdu"
-                                value="<?php echo $modelPojazdu; ?>" required data-validation>
+                                value="<?php echo $query2['modelPojazdu']; ?>" required data-validation>
                             </div>
                     
                         </div>
@@ -60,24 +74,24 @@
                             <div class="col-md-4">
                                 <label class="w3-text-green" ><b>Opis usterki:</b></label>
                                 <input type="text" class="form-control" name="opisUsterki"
-                                value="<?php echo $opisUsterki; ?>" required data-validation>
+                                value="<?php echo $query2['opisUsterki']; ?>" required data-validation>
                             </div>
                             <div class="col-md-4">
                                 <label class="w3-text-green" ><b>Opis zlecenia:</b></label>
                                 <input type="text" class="form-control" name="opisZlecenia"
-                                value="<?php echo $opisZlecenia; ?>" required data-validation>
+                                value="<?php echo $query2['opisZlecenia']; ?>" required data-validation>
                             </div>
                             <div class="col-md-4">
                                 <label class="w3-text-green" ><b>Data Przekazania Pojazdu:</b></label>
-                                <input type="text" class="form-control" name="dataPrzekazaniaPojazdu"
-                                value="<?php echo $dataPrzekazaniaPojazdu; ?>" required data-validation>
+                                <input type="date" class="form-control" name="dataPrzekazania" class="form-control"
+                                value="<?php echo $query2['dataPrzekazania']; ?>">
                             </div>
                         </div>
                         <br>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="w3-btn" data-dismiss="modal">Close</button>
-                        <input class="w3-btn w3-green" type="submit" value="Zapisz" name="zapisz" form="modal-form">
+                        <button type="button" class="w3-btn" data-dismiss="modal">Anuluj</button>
+                        <input class="w3-btn w3-green" type="submit" value="Zapisz" name="zapisz" form="modal-form2">
                     </div>
                 </form>
             </div>
--- zleceniaKlient.php
+++ zleceniaKlient.php
@@ -3,41 +3,6 @@
     otworzPoloczenie();
 ?>
 
-<?php
-        if(isset($_POST['zapiszKlient'])){
-            //pobieramy dane z pól
-            $imie=$_POST['imie'] ?? '';
-            $password = $_POST['password'] ?? '';
-            $password2 = $_POST['password2'] ?? '';
-            $nazwisko=$_POST['nazwisko'] ?? '';
-            $email=$_POST['email'] ?? '';
-            $telefon=$_POST['telefon'] ?? '';
-            $kodPocztowy=$_POST['kodPocztowy'] ?? '';
-            $miasto=$_POST['miasto'] ?? '';
-            $ulica=$_POST['ulica'] ?? '';
-            $nrDomu=$_POST['nrDomu'] ?? '';
-            $nazwaFirmy=$_POST['nazwaFirmy'] ?? '';
-            $nip=$_POST['nip'] ?? '';
-
-            
-            if($password != $password2){
-                echo '<p>Podane hasła różnią się od siebie.</p>';
-            } else {
-                    $password = hashHaslo($_POST['password']);
-                    // i wykonujemy zapytanie na edycje usera
-                    $sql="UPDATE Klienci set imie='$imie',haslo='$password',nazwisko='$nazwisko',email='$email',telefon='$telefon'
-                        ,nrDomu='$nrDomu',kodPocztowy='$kodPocztowy', ulica='$ulica',miasto='$miasto',nazwaFirmy='$nazwaFirmy',nip='$nip'
-                        where idKlienta=$idKlienta";
-                    mysqli_query($polaczenie, $sql);
-            }
-        }
-
-        $idKlienta=$_SESSION['idKlienta'] ?? '';
-
-        $select="select imie, nazwisko, email, telefon, nrDomu, kodPocztowy, ulica, miasto, nazwaFirmy, nip FROM Klienci where idKlienta=$idKlienta";
-        $result=mysqli_query($polaczenie, $select);     
-        $query=mysqli_fetch_array($result);
-    ?>
 
 
 <div class="container">
@@ -50,22 +15,15 @@
             </form>
             <br>
             <form>
-                <input type="button" value="Moje Zlecenia" onclick="window.location.href='zleceniaKlient.php'" 
+                <input type="button" value="Moje Zlecenia" onclick="window.location.href='zleceniaKlient.php.php'" 
                 class="w3-btn w3-green" />
             </form>
             <br>
             <button type="button" class="w3-btn w3-green" data-toggle="modal" data-target=".bd-example-modal-lg">Moje dane</button>
         </div>
 
-        <!-- było 
-            <form class="col-md-10" method="post" action="zlecenieKlient.php" > 
-        -->
-        <form class="col-md-10" method="post" action="zleceniaKlient.php" >
-            <div class="row featurette">
-
-
-
-                <?php
+        <div class="col-md-10">
+        <?php
                     if(isset($_POST['deleteZlecenie'])) {
                         global $polaczenie;
                         $idZlecenia = $_POST['idZlecenia'];
@@ -80,7 +38,7 @@
                 $sql="SELECT idZlecenia, dataDodania, statusZlecenia, wartoscZlecenia FROM Zlecenia WHERE idKlienta=$idKlienta";
                     $result = mysqli_query($polaczenie, $sql);
                 echo '<div class="container">
-                <form class="col-md-12" method="post" action="wycena.php" >
+                <form class="col-md-12" method="post" action="zleceniaKlient.php" >
                 <div class="row featurette">
                     <table class="table">
                     <thead class="thead-light">
@@ -115,122 +73,13 @@
                     </table>
                 </div>
                 </form>
-                </div>
-
-                            
-                            </div>
-                        </form>
-
-
-                    </div>
-            </div>
-
-    <!--   TUTAJ MODAL DLA EDYCJI ZLECENIA  -->
-
-    <?php
-        $idZlecenia = $_POST['idZlecenia'] ?? '';
-        if(isset($_POST['zapisz'])){
-            //pobieramy dane z pól
-            $markaPojazdu = $_POST['markaPojazdu'] ?? '';
-            $modelPojazdu = $_POST['modelPojazdu'] ?? '';
-            $opisUsterki = $_POST['opisUsterki'] ?? '';
-            $opisZlecenia = $_POST['opisZlecenia'] ?? '';
-            $dataPrzekazaniaPojazdu = $_POST['dataPrzekazaniaPojazdu'] ?? '';
-
-            $sql="UPDATE Zlecenia set opisZlecenia='$opisZlecenia',opisUsterki='$opisUsterki',
-            markaPojazdu='$markaPojazdu',modelPojazdu='$modelPojazdu'
-                    where idZlecenia=$idZlecenia";
-                
-                mysqli_query($polaczenie, $sql);
-        }
-        /*
-        $select="select imie, nazwisko, email, telefon, nrDomu, kodPocztowy, ulica, miasto, nazwaFirmy, nip FROM Klienci where idKlienta=$idKlienta";
-        $result=mysqli_query($polaczenie, $select);     
-        $query=mysqli_fetch_array($result);
-        */
-    ?>
-
-
-    <div class="modal fade bd-example2-modal-lg" tabindex="-1" role="dialog" 
-    aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-
-    <?php 
-            $idKlienta =$_SESSION['idKlienta'] ?? '';
-            $sql2="SELECT idZlecenia, dataDodania, statusZlecenia, wartoscZlecenia FROM Zlecenia WHERE idKlienta=$idKlienta";
-            $result2 = mysqli_query($polaczenie, $sql2);
-    ?>
-
-    <div class="modal-dialog modal-lg" role="dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Edycja danych Zlecenia</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-
-            <div class="modal-body">
-                <!-- Zawartosc modala -->
-                <form method="post" action="zleceniaKlient.php" id="modal-form2">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <label class="w3-text-green" ><b>idZlecenia:</b></label>
-
-                                <input type="text" class="form-control" name="imie"
-                                value="<?php echo $query['idZlecenia']; ?>" readonly>
-
-                            </div>
-                            <div class="col-md-4">
-                                <label class="w3-text-green" ><b>Marka pojazdu:</b></label>
-                                <input type="text" class="form-control" name="markaPojazdu"
-                                value="<?php echo $query['markaPojazdu']; ?>" required data-validation>
-
-                            </div>
-                            <div class="col-md-4">
-                                <label class="w3-text-green" ><b>Model pojazdu:</b></label>
-                                <input type="text" class="form-control" name="modelPojazdu"
-                                value="<?php echo $query['modelPojazdu']; ?>" required data-validation>
-                            </div>
-                    
-                        </div>
-
-                        <br>
-
-                        <div class="row">
-                            <div class="col-md-4">
-                                <label class="w3-text-green" ><b>Opis usterki:</b></label>
-                                <input type="text" class="form-control" name="opisUsterki"
-                                value="<?php echo $query['opisUsterki']; ?>" required data-validation>
-                            </div>
-                            <div class="col-md-4">
-                                <label class="w3-text-green" ><b>Opis zlecenia:</b></label>
-                                <input type="text" class="form-control" name="opisZlecenia"
-                                value="<?php echo $opisZlecenia; ?>" required data-validation>
-                            </div>
-                            <div class="col-md-4">
-                                <label class="w3-text-green" ><b>Data Przekazania Pojazdu:</b></label>
-                                <input type="text" class="form-control" name="dataPrzekazaniaPojazdu"
-                                value="<?php echo $dataPrzekazaniaPojazdu; ?>" required data-validation>
-                            </div>
-                        </div>
-                        <br>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="w3-btn" data-dismiss="modal">Close</button>
-                        <input class="w3-btn w3-green" type="submit" value="Zapisz" name="zapisz" form="modal-form2">
-                    </div>
-                </form>
-            </div>
         </div>
-    </div>
-</div>
 
-
+    
 
 <?php
-    //include 'editZlecenieModal.php';
-    //include 'editKlient.php';
+    include 'editKlient.php';
+    include 'editZlecenieModal.php';
     include 'bottomPage.php';
-
+    zamknijPoloczenie(); 
 ?>  
