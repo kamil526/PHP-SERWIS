a3438462893c15531434c7d24c82f8d824b34435
michalSRT <m.sierotowicz89@gmail.com>
2019-02-07 23:30:15 +0100

    no message

---
+++ oFirmie.php
@@ -0,0 +1,28 @@
+<?php
+    include 'topPage.php';
+?>
+
+<div class="container">
+    <div class="col-md-10">
+
+        <h2 class="w3-text-green">O firmie:</h2>
+
+        <label> 
+        Szukasz sprawdzonego warsztatu samochodowego, któremu bez obaw możesz powierzyć swoje auto?<br>
+        Zapraszamy Cię do Nowego Sącza – gwarantujemy profesjonalną usługę serwisową, a stałym klientom oferujemy wysokie rabaty.<br>
+        <br>Naszą firmę tworzy zgrany zespół doświadczonych w branży mechaniki samochodowej pracowników, których wspierają wysokiej jakości narzędzia i urządzenia renomowanych firm, 
+        co jest gwarancją sprawnej oraz szybkiej obsługi klientów.<br>
+        <br>Wszystkie naprawy objęte są gwarancją, dzięki czemu klienci mogą czuć się pewnie po opuszczeniu bram naszego warsztatu samochodowego.
+        Naprawiając samochody, dążymy do usunięcia przyczyny usterki, a nie tylko jej skutków.<br>
+        <br>Używamy oryginalnych części zamiennych producenta lub zamienników, począwszy od najtańszych, a skończywszy na częściach OEM (dostawca części oryginalnych).<br>
+        <br>Zapewniamy, że wszystkie części zakupione w naszym warsztacie samochodowym zawsze są tańsze niż w innych sklepach.
+        </label>
+            
+    </div>
+</div>
+   
+
+<?php
+    include 'bottomPage.php';
+    
+?>  
