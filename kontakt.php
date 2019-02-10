88831106e7e8ee5bd8a83a08f817ea2c0321d0b6
kamil526 <kporeba526@gmail.com>
2019-02-10 02:25:26 +0100

    czytelność kodu, modyfikacja divów

--- kontakt.php
+++ kontakt.php
@@ -9,48 +9,44 @@
 </div>
 
 <div class="container">
-<div class="row">
-    <div class="col-md-5">
-        <h4>Siedziba firmy:</h4>
-        <label>
-        ul. Lwowska 1<br>
-        33-300 Nowy Sącz<br>
-        </label>
-        <br><hr>
-        <h4>Godziny otwarcia:</h4>
-        <label>
-        Poniedziałek - Piątek<br>
-        8:00 - 16:00<br>
-        </label>
-        <br><hr>
-        <h4>Dane kontaktowe:</h4>
-        <label>
-        Tel: +48 666 666 66<br>
-        e-mail: eSerwis@gmail.com<br>
-        </label>
+    <div class="row">
+            <div class="col-md-5">
+                <h4>Siedziba firmy:</h4>
+                <label>
+                ul. Lwowska 1<br>
+                33-300 Nowy Sącz<br>
+                </label>
+                <br><hr>
+                <h4>Godziny otwarcia:</h4>
+                <label>
+                Poniedziałek - Piątek<br>
+                8:00 - 16:00<br>
+                </label>
+                <br><hr>
+                <h4>Dane kontaktowe:</h4>
+                <label>
+                Tel: +48 666 666 66<br>
+                e-mail: eSerwis@gmail.com<br>
+                </label>
 
+                <p>&nbsp;</p>
+            </div>
 
-<p>&nbsp;</p>
+            <div class="col-md-7">      
+                <h3>Mapka dojazdu</h3>
+                <div>
+                    <script src="//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+                    <style>
+                        #map-canvas img{max-width:none}
+                    </style>
+                    <div id="map-canvas" style="width:100%; height:350px; margin:auto; padding:0;">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2584.572473532354!2d20.69065921543073!3d49.62466415440042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473de54b5266d9b3%3A0x7eb1171bcbd5c055!2sLwowska+1%2C+33-300+Nowy+S%C4%85cz!5e0!3m2!1spl!2spl!4v1549629148702" 
+                        width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <br><br><br><br>
+            </div>  
     </div>
-    <div class="col-md-7">
-                
-        <h3>Mapka dojazdu</h3>
-        <div>
-            <script src="//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
-            
-            <style>
-                                #map-canvas img{max-width:none}
-            </style>
-            <div id="map-canvas" style="width:100%; height:350px; margin:auto; padding:0;">
-
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2584.572473532354!2d20.69065921543073!3d49.62466415440042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473de54b5266d9b3%3A0x7eb1171bcbd5c055!2sLwowska+1%2C+33-300+Nowy+S%C4%85cz!5e0!3m2!1spl!2spl!4v1549629148702" 
-            width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-		</div>
-        </div>
-    </div>
-</div>
-</div>
-<br><br><br><br>
 </div>
 
 <?php
